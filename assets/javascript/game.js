7e3452cf3e26b7c2e7063be48a5b0bf7b1627982
robertalanis <rxalanis@gmail.com>
2020-04-06 16:22:42 -0500

    responsive layout and added firebase

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyB9n3s3BVpOXwDnXTP7qrCoBhU9zmEL31Y",
+    authDomain: "rps-game-d378e.firebaseapp.com",
+    databaseURL: "https://rps-game-d378e.firebaseio.com",
+    projectId: "rps-game-d378e",
+    storageBucket: "rps-game-d378e.appspot.com",
+    messagingSenderId: "759467326977",
+    appId: "1:759467326977:web:73058e622884ecf1830a00",
+    measurementId: "G-G47C58RQ89"
+};
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
